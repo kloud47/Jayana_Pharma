2c8359dc9db07d876e81d62abaf433eaef0e7f70
kloud47 <136368351+kloud47@users.noreply.github.com>
2024-11-14 00:41:58 +0530

    Added new products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,444 @@
+import MaxWidthWrapper from "@/components/global/MaxWidthWrapper";
+import { CircleDot, GitCommitVertical, Pointer } from "lucide-react";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="font-serif">
+      <MaxWidthWrapper>
+        <div className="mt-10 grid grid-cols-[40%_60%] h-[30vh] border-b border-primary">
+          <h1 className="text-6xl font-bold uppercase bg-primary/20 flex items-center justify-center p-4">
+            Our Products
+          </h1>
+          <div className="BGProd"></div>
+        </div>
+        <div className="grid grid-cols-4 my-20 border-b border-primary text-lg font-thin font-sans py-2 bg-muted">
+          <span className="flex">
+            <GitCommitVertical />
+            Ambroxol
+          </span>
+          <span>Aspirin</span>
+          <span>Bronopol</span>
+          <span>Caffeine Citrate</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Alluminium Chloride
+          </span>
+          <span>Cetrimide</span>
+          <span>Clopidogrel Bi Sulphate</span>
+          <span>Chlorzoxazone</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Calcium Glycero Phosphate
+          </span>
+          <span>Dextromethorphan HBR</span>
+          <span>Calcium Levulinate</span>
+          <span>Drotravarine</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Di-Iodohydroxyquinoline
+          </span>
+          <span>Calcium D Saccharate</span>
+          <span>Doxofylline</span>
+          <span>Sodium Glycero Phosphate</span>
+          <span className="flex">
+            <GitCommitVertical />
+            E/H Gelatine Capsules
+          </span>
+          <span>Ferrous Fumarate</span>
+          <span>Flavoxate HCL</span>
+          <span>Guaifenesin</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Potassium Iodide
+          </span>
+          <span>Iodine</span>
+          <span>Potassium Iodate</span>
+          <span>Ketoconazole</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Lignocaine HCL
+          </span>
+          <span>Chlorhexidine Gluconate</span>
+          <span>Menthol</span>
+          <span>Methyl Cobalamin</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Benfothiamine
+          </span>
+          <span>Mometasone Furoate</span>
+          <span>Ofloxacin</span>
+          <span>Pantaprazole Sodium</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Paracetamol
+          </span>
+          <span>Famotidine</span>
+          <span>Potassium Chloride</span>
+          <span>Prednisolone</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Propyl Gallate
+          </span>
+          <span>Montelukast Sodium</span>
+          <span>Rabeprazole Sodium</span>
+          <span>Resorcinol</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Salicylic Acid
+          </span>
+          <span>Sertraline HCL</span>
+          <span>Silver Sulphadiazine</span>
+          <span>Sodium Chloride</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Gentian Violet
+          </span>
+          <span>Iodoform</span>
+          <span>Sodium Saccharin</span>
+          <span>Merbromin</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Pharma Resins
+          </span>
+          <span>Surcalfate</span>
+          <span>Terbinafine HCL</span>
+          <span>Proflavine Hemisulphate</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Trimethoprim
+          </span>
+          <span>Sulphamethaxazole</span>
+          <span>Sulfasalazine</span>
+          <span>Formic Acid</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Metrnidazole Benzoate
+          </span>
+          <span>Nitazoxanide</span>
+          <span>Ornidazole</span>
+          <span>Secnidazole</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Tinidazole
+          </span>
+          <span>Ciprofloxacin</span>
+          <span>Enrofloxacin</span>
+          <span>Erythromycin &amp; Salts</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Roxithromycin
+          </span>
+          <span>Sparfloxacin</span>
+          <span>Amoxycillin T/H Comp</span>
+          <span>Ampicillin T/H</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Dicloxacillin Sodium
+          </span>
+          <span>Flucloxacillin Sodium</span>
+          <span>Amoxycillin Extra Dry</span>
+          <span>Hyoscine Butyl Bromide</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Thicolchicoside
+          </span>
+          <span>Di Ethyl Carbamazine Citrate</span>
+          <span>Tramadol</span>
+          <span>Artemether</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Lumefantrine
+          </span>
+          <span>Quinine Di-HCL</span>
+          <span>Quinine Sulphate</span>
+          <span>Tranexamic Acid</span>
+          <span className="flex">
+            <GitCommitVertical />
+            Levofloxacin
+          </span>
+        </div>
+        <div className="border-b border-primary mb-5">
+          <h1 className="text-center text-4xl">Excipients</h1>
+          <div className="grid grid-cols-3 font-sans font-thin text-lg mt-5 bg-muted p-2">
+            <span className="flex">
+              <GitCommitVertical />
+              Aloe Vera
+            </span>
+            <span>Cellulose Acetate Phthalate</span>
+            <span>Cab-O-Sil</span>
+            <span className="flex">
+              <GitCommitVertical />
+              Carbopol 934/940
+            </span>
+            <span>Calamine</span>
+            <span>Citric Acid Anhydrous/Mono</span>
+            <span className="flex">
+              <GitCommitVertical />
+              Cross Carmellose Sodium
+            </span>
+            <span>Di Basic Calcium Phosphate</span>
+            <span>EDTA Di-Sodium</span>
+            <span className="flex">
+              <GitCommitVertical />
+              Eudragit L-100/S 100
+            </span>
+            <span>Glycerin</span>
+            <span>Guar Gum</span>
+            <span className="flex">
+              <GitCommitVertical />
+              Gum Accacia
+            </span>
+            <span>I.P.A</span>
+            <span>Light Kaolin</span>
+            <span className="flex">
+              <GitCommitVertical />
+              Tri-Choline Cit.
+            </span>
+            <span>Talc</span>
+            <span>Lactose</span>
+            <span className="flex">
+              <GitCommitVertical />
+              M.C.C.P (All Grades)
+            </span>
+            <span>Methyl Salicylate</span>
+            <span>Magnesium Stearate</span>
+            <span className="flex">
+              <GitCommitVertical />
+              Methylene Chloride
+            </span>
+            <span>Methyl Paraben Plain</span>
+            <span>Methyl Paraben Sodium</span>
+            <span className="flex">
+              <GitCommitVertical />
+              Propyl Paraben Plain
+            </span>
+            <span>Propyl Paraben Sodium</span>
+            <span>Propylene Glycol</span>
+            <span className="flex">
+              <GitCommitVertical />
+              Sodium Benzoate
+            </span>
+            <span>Sodium Starch Glycolate</span>
+            <span>Sorbitol</span>
+            <span className="flex">
+              <GitCommitVertical />
+              Sodium Salicylate
+            </span>
+            <span>Starch</span>
+            <span>Simethicone Emulsions</span>
+          </div>
+          <div className="flex justify-center gap-20 bg-muted pt-5 pb-2">
+            <div>
+              <h1 className="text-2xl">Emulsifiers</h1>
+              <div className="flex flex-col font-sans font-thin text-lg mt-2">
+                <span>Carnauba Wax</span>
+                <span>Yellow Soft Paraffin</span>
+                <span>Hard Paraffin Wax</span>
+              </div>
+            </div>
+            <div>
+              <h1 className="text-2xl">Natural Oils</h1>
+              <div className="flex flex-col font-sans font-thin text-lg mt-2">
+                <span>Eucalyptus Oil</span>
+                <span>Dill Oil</span>
+                <span>Carragenan Oil</span>
+                <span>Linseed Oil</span>
+                <span>Anise Oil/ Arachis Oil</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="border-b border-primary my-20">
+          <h1 className="text-center text-4xl">Therapeutic Use</h1>
+          <div className="grid grid-cols-3 pt-4">
+            <div className="grid grid-cols-2 bg-muted border-b border-primary">
+              <div className="bg-primary/10 flex flex-col justify-center items-center text-lg">
+                <span>Anti Inflammatory</span>
+              </div>
+              <div className="flex flex-col p-2">
+                <span>Aceclofenac</span>
+                <span>Diclofenac Sodium</span>
+                <span>Diclofenac Potassium</span>
+                <span>Diclofenac Diethyl Amine</span>
+                <span>Ibuprofen</span>
+                <span>Mefenamic Acid</span>
+                <span>Nimesulide</span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 bg-muted border-b border-primary">
+              <div className="bg-primary/10 flex flex-col justify-center items-center text-lg">
+                <span>Disinfectant Chemicals</span>
+              </div>
+              <div className="flex flex-col p-2">
+                <span>Aceclofenac</span>
+                <span>Diclofenac Sodium</span>
+                <span>Diclofenac Potassium</span>
+                <span>Diclofenac Diethyl Amine</span>
+                <span>Ibuprofen</span>
+                <span>Mefenamic Acid</span>
+                <span>Nimesulide</span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 bg-muted border-b border-primary">
+              <div className="bg-primary/10 flex flex-col justify-center items-center text-lg">
+                <span>Veterinary & Aqua</span>
+              </div>
+              <div className="flex flex-col p-2">
+                <span>Aceclofenac</span>
+                <span>Diclofenac Sodium</span>
+                <span>Diclofenac Potassium</span>
+                <span>Diclofenac Diethyl Amine</span>
+                <span>Ibuprofen</span>
+                <span>Mefenamic Acid</span>
+                <span>Nimesulide</span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 bg-muted">
+              <div className="bg-primary/10 flex flex-col justify-center items-center text-lg">
+                <span>Anti-Diabetic</span>
+              </div>
+              <div className="flex flex-col p-2">
+                <span>Gliclazide</span>
+                <span>Glimepiride</span>
+                <span>Metformin HCL</span>
+                <span>Pioglitazone</span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 bg-muted text-nowrap text-sm">
+              <div className="bg-primary/10 flex flex-col justify-center items-center text-lg">
+                <span>Anti Ulcerative Pellets</span>
+              </div>
+              <div className="flex flex-col p-2">
+                <span>Esomeprazole Magnesium 8.5%</span>
+                <span>Lansoprazole 8.5%</span>
+                <span>Omeprazole 7.5%, 8.5%, 10%</span>
+                <span>Pantoprazole 15%</span>
+                <span>Rabeprazole 8.5%</span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 bg-muted text-nowrap">
+              <div className="bg-primary/10 flex flex-col justify-center items-center text-lg">
+                <span>Antihistamine</span>
+              </div>
+              <div className="flex flex-col p-2">
+                <span>Cinnarizine</span>
+                <span>Cetirizine</span>
+                <span>Diphenhydramine HCL</span>
+                <span>Flunarizine</span>
+                <span>Fexofenadine</span>
+                <span>Loratidine</span>
+                <span>Levocetrizine</span>
+                <span>Promethazine HCL</span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 bg-muted text-nowrap border-t border-primary">
+              <div className="bg-primary/10 flex flex-col justify-center items-center text-lg">
+                <span>Vitamins</span>
+              </div>
+              <div className="flex flex-col p-2">
+                <span>Calcium D Pantohenate</span>
+                <span>Cynocobalamin</span>
+                <span>Niacinamide</span>
+                <span>Pyridoxine HCL</span>
+                <span>Riboflavin</span>
+                <span>Thiamine HCL</span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 bg-muted text-nowrap border-t border-primary">
+              <div className="bg-primary/10 flex flex-col justify-center items-center text-lg">
+                <span>Anti Diarrhoeals</span>
+              </div>
+              <div className="flex flex-col p-2">
+                <span>Diloxanide Furoate</span>
+                <span>Metronidazole</span>
+                <span>Metronidazole Benzoate</span>
+                <span>Nitazoxanide</span>
+                <span>Ornidazole</span>
+                <span>Secnidazole</span>
+                <span>Tinidazole</span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 bg-muted text-nowrap border-t border-primary">
+              <div className="bg-primary/10 flex flex-col justify-center items-center text-lg">
+                <span>Steroids</span>
+              </div>
+              <div className="flex flex-col p-2">
+                <span>Betamethasone Salts</span>
+                <span>Dexamethasone Salts</span>
+                <span>Prednisolone</span>
+                <span>Erythromycin Salts</span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 bg-muted text-nowrap border-t border-primary">
+              <div className="bg-primary/10 flex flex-col justify-center items-center text-lg">
+                <span>AntiBiotic</span>
+              </div>
+              <div className="flex flex-col p-2">
+                <span>Ciprofloxacin</span>
+                <span>Enrofloxacin</span>
+                <span>Sparfloxacin</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="border-b border-primary my-20">
+          <h1 className="text-center text-4xl">Imports & Indents</h1>
+          <ul className="grid grid-cols-2 mx-10">
+            <li className="flex gap-2">
+              <CircleDot fill="black" />
+              <div className="flex flex-col">
+                Iodine
+                <span>(SQM EURPOE N.V.)</span>
+              </div>
+            </li>
+            <li className="flex gap-2">
+              <CircleDot fill="black" />
+              <div className="flex flex-col">
+                Doxycycline HCL
+                <span>(HEBEI JIUPENG PHARMACEUTICAL CO. LTD. )</span>
+              </div>
+            </li>
+            <li className="flex gap-2">
+              <CircleDot fill="black" />
+              <div className="flex flex-col">
+                Norfloxacin
+                <span>(Henan Kangtai Pharmaceutical Group Corporation)</span>
+              </div>
+            </li>
+            <li className="flex gap-2">
+              <CircleDot fill="black" />
+              <div className="flex flex-col">
+                Azithromycin, Roxithromycin, Ciprofloxacin, Clarithromycin
+                <span>(Zhejiang Guobang Pharmaceuticals Pvt. Ltd.)</span>
+              </div>
+            </li>
+            <li className="flex gap-2">
+              <CircleDot fill="black" />
+              <div className="flex flex-col">
+                Vitamin E-Liq
+                <span>(Zhejiang Medicine Co. Ltd.)</span>
+              </div>
+            </li>
+            <li className="flex gap-2">
+              <CircleDot fill="black" />
+              <div className="flex flex-col">
+                Norethistrone, Prednisolone, Mifepristone, Dexamethasone
+                <span>(Tianjin Tianmao Technology Development Corp. Ltd.)</span>
+              </div>
+            </li>
+            <li className="flex gap-2">
+              <CircleDot fill="black" />
+              <div className="flex flex-col">
+                Levamisole HCL Vet
+                <span>(Zhejiang Medicine Co. Ltd.)</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </MaxWidthWrapper>
+    </div>
+  );
 };
 
 export default page;
